94ed9b0aaf2f1256e7c064f7e507ea1aa20982e3
darrylbalderas <drrylbalderas@gmail.com>
2019-04-20 19:13:36 -0700

    Finish flow for simon game
    Signed-off-by: darrylbalderas <drrylbalderas@gmail.com>

--- simon/simon.py
+++ simon/simon.py
@@ -1,19 +1,126 @@
+import os
+import sys
+import platform
+import time
+import random
 
-def continue_game() -> bool:
-    choice = input("Want to play \
-this game (Hit any key to continue or 'q' to quit): ")
+
+def continue_game(play_again=False) -> bool:
+    if play_again:
+        message = "Want to play \
+again this game (Hit any key to continue or 'q' to quit): "
+    else:
+        message = "Want to play \
+(Hit any key to continue or 'q' to quit): "
+        
+    choice = input(message)
     return True if choice.lower() != 'q' else False
 
-def main():
-    header = """
- .----..-..-.   .-. .----. .-. .-.    .---.   .--.  .-.   .-..----.
-{ {__  | ||  `.'  |/  {}  \|  `| |   /   __} / {} \ |  `.'  || {_  
-.-._} }| || |\ /| |\      /| |\  |   \  {_ }/  /\  \| |\ /| || {__ 
-`----' `-'`-' ` `-' `----' `-' `-'    `---' `-'  `-'`-' ` `-'`----'
-    """
+
+def breaks(n=0) -> str:
+    return "\n"*n
+
+
+def clear_output(wait_time=0) -> None:
+    time.sleep(wait_time)
+    if 'linux' in sys.platform or 'darwin' == sys.platform:
+        os.system('clear') 
+    else:
+        os.system('cls') 
+
+
+def level_message(sequence: list, level: int) -> None:
+    message = "\033[1;95mSequence {}:\n\n\033[4m{}".format(level, " ".join(map(str, sequence)))
+    return message
+
+
+def reset_term_colors():
+    print("\33[0m")
+
+
+def print_message(message="", sleep_time=2):
+    header = """\033[1;32;40m
+ .----..-..-.   .-. .----. .-. .-.    .---.   .--.  .-.   .-..----.  ||| |||
+{ {__  | ||  `.'  |/  {}  \|  `| |   /   __} / {} \ |  `.'  || {_    ||| |||
+.-._} }| || |\ /| |\      /| |\  |   \  {_ }/  /\  \| |\ /| || {__   ||| |||
+`----' `-'`-' ` `-' `----' `-' `-'    `---' `-'  `-'`-' ` `-'`----'   0   0
+"""
+    clear_output()
     print(header)
-    while(continue_game()):
-        pass
+    reset_term_colors()
+    if message:
+        print(message)
+    time.sleep(sleep_time)
+    clear_output()
+    reset_term_colors()
+
+
+def create_simon(level: int) -> list:
+    return random.randint(1, 4)
+
+
+def print_failing_index(index):
+    space = (index)*' '
+    print(space + '^')
+
+
+def input_error_message(invalid_msg, index, answers):
+    print(invalid_msg + answers)
+    print_failing_index(index + len(invalid_msg))
+
+
+def validate_answers(answers) -> tuple:
+    invalid_msg = "Answers can only be numbers from 1 through 4: "
+    for index, value in enumerate(answers):
+        try:
+            seq_num = int(value)
+            if seq_num < 0 or seq_num > 4:
+                input_error_message(invalid_msg, index, answers)
+                return index, False
+        except:
+            input_error_message(invalid_msg, index, answers)
+            return index, False
+    return None, True
+
+
+def is_correct(level, user):  
+    if len(level) != len(user):
+        return False
+    for i in range(len(level)):
+        if level[i] != user[i]:
+            return False
+    return True
+
+
+def main():
+    print_message()
+    lost = False
+    level = 1
+    play_game = continue_game()
+    while play_game:
+        print(breaks())
+        print_message("Let's get started !!!!")
+        sequence = []
+        while not lost:
+            sequence.append(create_simon(level))
+            print_message(level_message(sequence, level))
+            valid = False
+            user_answer = []
+            while not valid:
+                clear_output(1)
+                user_input = input("Type sequence: ")
+                index, valid,  = validate_answers(user_input)
+            user_answer = [int(val) for val in user_input ]
+            print(sequence)
+            print(user_answer)
+            print(is_correct(sequence, user_answer))
+            if not is_correct(sequence, user_answer):
+                lost = True
+                print("Sorry you lost !!!")
+            level += 1
+        if lost:
+            play_game = continue_game()
+            lost = False
 
 
 if __name__ == "__main__":
--- simon/tests/test_simon.py
+++ simon/tests/test_simon.py
@@ -10,6 +10,27 @@ class TestSimon(unittest.TestCase):
     @patch('builtins.input', return_value='a')
     def test_continue_game(self, random_input):
         self.assertEqual(simon.continue_game(), True)
+    
+    def test_breaks(self):
+        self.assertEqual(simon.breaks(3), "\n\n\n")
+        self.assertEqual(simon.breaks(0), "")
+    
+    def test_game_header(self):
+        self.assertEqual(simon.level_message([1,2,3], 1), "\033[1;95mSequence 1:\n\n\033[4m1 2 3")
+    
+    def test_validate_answer(self):
+        self.assertEqual(simon.validate_answers('1234'), (None, True))
+        self.assertEqual(simon.validate_answers('123a'), (3,False))
+        self.assertEqual(simon.validate_answers('a23a'), (0, False))
+        self.assertEqual(simon.validate_answers('15349'), (1, False))
+        self.assertEqual(simon.validate_answers('12 349'), (2,False))
+        self.assertEqual(simon.validate_answers('13""49'), (2,False))    
+        self.assertEqual(simon.validate_answers('4321'), (None, True))
+    
+    def test_check_answers(self):
+        self.assertEqual(simon.is_correct([1,2,3], [1,3,4]), False)
+        self.assertEqual(simon.is_correct([1,2,3], [1,2,3]), False)
+        self.assertEqual(simon.is_correct([1,2], [1,2,3]), True)
 
 
 if __name__ == "__main__":
